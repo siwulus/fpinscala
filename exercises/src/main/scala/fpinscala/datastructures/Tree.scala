66e2226037b29fefe03e674ba76133178dc06279
siwy <siwulus@gmail.com>
2016-02-02 22:53:32 +0100

    trees pocztek

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,12 +1,22 @@
 package fpinscala.datastructures
 
-sealed trait Tree[+A]
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+sealed trait Tree[+A]{
+  def size(): Int
+}
+case class Leaf[A](value: A) extends Tree[A]{
+  def size() = 1
+}
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]{
+  def size() = left.size() + right.size() + 1
+}
 
 
 object Tree {
-
+  def size[A](t: Tree[A]): Int = t match {
+      case l: Leaf => 1
+      case b: Branch => size(b.left) + size(b.right) + 1
+  }
 
 
 
