0ae92a4e2551b00a8b446e873d30bac0c09a1e09
two <two@hicron.com>
2016-02-12 11:06:30 +0100

    either

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,49 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+ def map[B](f: A => B): Either[E, B] = this match{
+    case Right(a) => Right(f(a))
+    case _ => _
+ }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match{
+   case Right(a) => f(a)
+   case _ => _
+ }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match{
+   case Left(_) => b
+   case _ => _
+ }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+  this flatMap((a) => b map(f(a, _)))
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = sys.error("todo")
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight(Right(Nil):Either[E, List[B]])((a, b) => f(a).map2(b)(_::_))
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = sys.error("todo")
+  def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match{
+    case h::t => f(h).map2(traverse_1(t)(f))(_::_)
+    case Nil => Right(Nil)
+  }
+
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    es.foldRight(Right(Nil):Either[E, List[A]])((e, z) => e.map2(z)(_::_))
+
+  def sequence_1[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(a => a)
+
+  def sequence_2[E,A](es: List[Either[E,A]]): Either[E,List[A]] = es match{
+    case h::t => h.map2(sequence_2(t))(_::_)
+    case Nil => Right(Nil)
+  }
+
+
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
